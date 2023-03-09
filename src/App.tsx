e1fa4f6d34818a6ae5cada6a9a63842a3bc4866f
Jason Kurian <JaKXz@users.noreply.github.com>
2023-03-09 18:46:14 -0500

    initial solve for showing conflicts
    and visual cleanup, showing which column is being sorted by

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import { EVENTS } from "./data";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       {EVENTS.length > 0 ? <Events list={EVENTS} /> : "There were no events :("}
     </div>
   );
--- src/Events.tsx
+++ src/Events.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 
-import { CalendarEvent } from "./data";
+import type { CalendarEvent } from "./data";
 
 type Props = {
   list: CalendarEvent[];
 };
+type EventMap = { [key: CalendarEvent["id"]]: { start: number; end: number } };
 
 export function Events({ list }: Props) {
   const [sorted, setSorted] = React.useState(list);
@@ -20,39 +21,76 @@ export function Events({ list }: Props) {
 
     if (prev === current) {
       setSorted((tempList) => [...tempList].reverse());
-    } else {
+    } else if (current) {
       setSorted((tempList) =>
-        [...tempList].sort((a, b) => a.title.localeCompare(b.title))
+        [...tempList].sort((a, b) => a[current] != null && a[current].localeCompare(b[current]))
       );
     }
 
     setSortingBy(current);
   }
 
+  const eventMap: EventMap = list.reduce(
+    (acc, event) => ({
+      ...acc,
+      [event.id]: {
+        start: new Date(event.start).getTime(),
+        end: event.end === null ? Infinity : new Date(event.end).getTime()
+      }
+    }),
+    {}
+  );
+
   return (
     <table>
       <thead>
-        {Object.keys(list[0])
-          .map(
-            (header) =>
-              header !== "id" && (
-                <th onClick={handleColumnClick} key={header}>
-                  {sortingBy === header ? <em>{header}</em> : header}
-                </th>
-              )
-          )
-          .filter(Boolean)}
+        <tr>
+          {Object.keys(list[0])
+            .map(
+              (header) =>
+                header !== "id" && (
+                  <th onClick={handleColumnClick} key={header}>
+                    {sortingBy === header ? <em>{header}</em> : header}
+                  </th>
+                )
+            )
+            .filter(Boolean)}
+        </tr>
       </thead>
       <tbody>
         {sorted.map((event) => (
-          <tr key={event.id}>
+          <tr key={event.id} className={allConflictingEvents(event, eventMap)}>
             <td>{event.title}</td>
             <td>{event.category}</td>
-            <td>{event.start}</td>
-            <td>{event.end === null ? "Present" : event.end}</td>
+            <td>{formatDate(event.start)}</td>
+            <td>{event.end === null ? "Present" : formatDate(event.end)}</td>
           </tr>
         ))}
       </tbody>
     </table>
   );
+}
+
+function formatDate(date: string) {
+  return new Intl.DateTimeFormat(undefined, {
+    dateStyle: "full",
+    timeStyle: "long",
+    timeZone: "PST"
+  }).format(new Date(date));
+}
+
+function allConflictingEvents(event: CalendarEvent, map: EventMap): string {
+  return Object.entries(map)
+    .filter(([eventId, { start, end }]) => {
+      if (String(event.id) === eventId) return false;
+      const { start: startTime, end: endTime } = map[event.id];
+      return (
+        // current event's end is between another event's points on the timeline
+        (endTime > start && endTime < end) ||
+        // current event's start is between another event's points on the timeline
+        (startTime < end && startTime > start)
+      );
+    })
+    .map(([eventId]) => `conflict-with-${eventId}`)
+    .join(" ");
 }
